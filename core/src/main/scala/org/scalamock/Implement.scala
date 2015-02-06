72e9c74ac990a4d80ee56e87da2fb21340b035d5
Paul Butcher <paul@paulbutcher.com>
2015-02-06 00:28:26 +0000

    Switch to using parse method

--- core/src/main/scala/org/scalamock/Implement.scala
+++ core/src/main/scala/org/scalamock/Implement.scala
@@ -23,16 +23,18 @@ object ImplementImpl {
     
     val methods = methodsToImplement map { m =>
         val info = m.infoIn(typeToImplement)
-        val name = m.name.toTermName
-        val tparams = info.typeParams map { p => q"type ${p.name.toTypeName}" }
+        val name = m.name
+        val tparams = info.typeParams match {
+          case Nil => ""
+          case tps => tps.map(_.name).mkString("[", ", ", "]")
+        }
         val res = info.finalResultType
-        val paramss = info.paramLists map {params => 
-          params map { param => 
-              q"val ${param.name.toTermName}: ${param.infoIn(typeToImplement)}"
-            }
-          }
+        val paramss = info.paramLists.map { ps =>
+            ps.map(p => s"${p.name}: ${p.infoIn(typeToImplement)}").mkString("(", ", ", ")")
+          }.mkString("")
+        val method = s"def $name$tparams$paramss = null.asInstanceOf[$res]"
         
-        q"override def $name[..$tparams](...$paramss): $res = null.asInstanceOf[$res]"
+        c.parse(method)
       }
     
     c.Expr[Any](q"""
